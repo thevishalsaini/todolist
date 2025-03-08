52f49786757251b18e5022021368746dab5f47c1
Vishal Saini <Vishalsaini9675@gmail.com>
2025-03-08 18:44:49 +0530

    remove comment

--- src/App.jsx
+++ src/App.jsx
@@ -54,11 +54,9 @@ function App() {
             Manage Your Todos
           </h1>
           <div className="mb-4">
-            {/* Todo form goes here */}
             <TodoForm />
           </div>
           <div className="flex flex-wrap gap-y-3">
-            {/*Loop and Add TodoItem here */}
             {todos.map((todo) => (
               <div key={todo.id} className="w-full">
                 <TodoItem todo={todo} />
